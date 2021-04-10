5961112509a5cc5359ba45559fd9c30ae40c1475
CarlosAlbertoR <carlosalbertorios3@gmail.com>
2021-04-10 18:38:33 -0500

    Header component was created.

--- src/app/pages/action/action.module.ts
+++ src/app/pages/action/action.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ActionPageRoutingModule } from './action-routing.module';
 
 import { ActionPage } from './action.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ActionPageRoutingModule
+    ActionPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ActionPage]
 })
